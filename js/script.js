fbff6f2c21b7f97ce9ea1713440b4ed03fdbcdad
divixtr3n <152651958+divixtr3n@users.noreply.github.com>
2024-01-13 14:09:28 +0100

    section about v2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
+	const allNavItems = document.querySelectorAll('.nav-link')
+	const navList = document.querySelector('.navbar-collapse')
 
 	function addShadow() {
 		if (window.scrollY >= 300) {
@@ -9,5 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 
-    window.addEventListener('scroll', addShadow)
+	allNavItems.forEach(item => item.addEventListener('click', () => {
+		navList.classList.remove('show')
+	}))
+
+	window.addEventListener('scroll', addShadow)
+
 })
